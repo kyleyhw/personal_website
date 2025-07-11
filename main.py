287be69979d96884edda07fc466873fdc2a38f56
Kyle W <kyleyhw@gmail.com>
2025-07-11 18:52:40 +0100

    Revert "modularized load_font() and renamed main to home"
    This reverts commit 0a39e0c8568fd88ea3be0152a6a0e5b92a2631d5.

--- main.py
+++ main.py
@@ -11,16 +11,14 @@ st.set_page_config(
 )
 
 # --- LOAD INTER FONT ---
-from load_style import *
-load_font()
-# st.markdown(
-#     """
-#     <style>
-#     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap');
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap');
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 # --- GITHUB AND ASSET SETUP ---
 GITHUB_USERNAME = "kyleyhw"
