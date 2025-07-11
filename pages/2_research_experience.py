e832d618a7bb5c8168b3ee2175507a04c462a7bc
Kyle <kyleyhw@gmail.com>
2025-07-12 00:03:44 +0100

    put headers in detail cards for research experience

--- pages/2_research_experience.py
+++ pages/2_research_experience.py
@@ -1,34 +1,37 @@
 import streamlit as st
 
-from load_style import *
-load_font()
+# --- LOAD INTER FONT ---
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap');
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 # --- RESEARCH & SKILLS PAGE CONTENT ---
-st.title("Research & Skills")
+st.title("research experience")
 
-st.header("Research Experience")
+st.header("research projects")
 
-st.subheader("Master's of Advanced Study Research Project")
-st.write("*Institute of Astronomy, University of Cambridge (Oct 2024 - Present)*")
-with st.expander("Details"):
+with st.expander(
+        "Master's of Advanced Study Research Project | *Institute of Astronomy, University of Cambridge (Oct 2024 - Present)*"):
     st.markdown("""
     - **Topic:** Implementation and statistical testing of variable initial conditions and resolution in the 21cmSPACE code package for simulating hydrogen gas clouds in the early universe.
     - **Goal:** To enable efficient forecasting for future radio astronomy experiments, particularly the Square Kilometre Array (SKA).
     - **Skills:** Simulation, MATLAB, High-Performance Computing, Linux, Data Visualization, Cosmology.
     """)
 
-st.subheader("Canadian Institute for Theoretical Astrophysics (CITA) Summer Fellowship")
-st.write("*University of Toronto (May 2023 - Dec 2023)*")
-with st.expander("Details"):
+with st.expander(
+        "Canadian Institute for Theoretical Astrophysics (CITA) Summer Fellowship | *University of Toronto (May 2023 - Dec 2023)*"):
     st.markdown("""
     - **Topic:** Probing neutron star tidal deformability from gravitational wave signals using Markov Chain Monte Carlo (MCMC) parameter estimation.
     - **Goal:** Incorporating new models for neutron star equation of state correlations into the LIGO Scientific Collaboration's analysis pipeline.
     - **Skills:** Simulation, MCMC, Machine Learning, Bayesian Inference, High-Performance Computing, Linux.
     """)
 
-st.subheader("McGill Space Institute Summer Research Award")
-st.write("*McGill University (May 2022 - Apr 2023)*")
-with st.expander("Details"):
+with st.expander("McGill Space Institute Summer Research Award | *McGill University (May 2022 - Apr 2023)*"):
     st.markdown("""
     - **Topic:** Incorporating statistical priors into the power spectrum data estimator for the Hydrogen Epoch of Reionization Array (HERA) collaboration.
     - **Goal:** To improve the analysis of radio astronomy data from HERA's cosmic dawn experiment.
@@ -37,12 +40,12 @@ with st.expander("Details"):
 
 st.write("---")
 
-st.header("Technical Skills")
+st.header("technical skills")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("Programming")
+    st.subheader("programming")
     st.markdown("""
     - Python
     - MATLAB
@@ -51,7 +54,7 @@ with col1:
     """)
 
 with col2:
-    st.subheader("Data Analysis & Simulation")
+    st.subheader("data analysis & simulation")
     st.markdown("""
     - Fast Fourier Transform (FFT)
     - Numerical Methods (differentiation, integration, root finding)
@@ -59,7 +62,7 @@ with col2:
     - Bayesian Inference
     """)
 
-st.subheader("Key Libraries & Software")
+st.subheader("key libraries & software")
 st.markdown("""
 - **Bilby:** Extensive use for parameter estimation.
 - **21cmSPACE:** Cosmological simulation package.
